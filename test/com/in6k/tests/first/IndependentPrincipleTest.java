3f706f276337946e1b4eeaad2e6d91dddc15c0a8
Zabudska-Yuliia <yuliiaz@softwareplanetgroup.com>
2016-11-01 17:45:36 +0200

    Get rid of isolated.

--- test/com/in6k/tests/first/IndependentPrincipleTest.java
+++ test/com/in6k/tests/first/IndependentPrincipleTest.java
@@ -4,20 +4,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import org.testng.annotations.Test;
 
-
 public class IndependentPrincipleTest {
-
-    public int result = 0;
 
     @Test
     public void test2() throws Exception {
-        result += 4;
-        assertThat(result, equalTo(6));
+        int result = 4;
+        assertThat(result, equalTo(4));
     }
 
     @Test
     public void test1() throws Exception {
-        result += 2;
+        int result = 2;
         assertThat( result, equalTo(2));
     }
 
